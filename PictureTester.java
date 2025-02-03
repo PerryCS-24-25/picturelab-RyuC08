4656d3fee59e74b3ee3135785ce0cbda73b06aa4
RyuC08 <rcheng2026@gmail.com>
2025-02-03 10:05:20 -0700

    Change photo in testExplorer()

--- PictureTester.java
+++ PictureTester.java
@@ -11,8 +11,9 @@ public class PictureTester {
      * Method to test the PictureExplorer
      */
     public static void testExplorer() {
-        Picture beach = new Picture("beach.jpg");
-        beach.explore();   
+        Picture bKarl = new Picture("blueKarl.jpg");
+        bKarl.explore();
+
     }
 
     /**
